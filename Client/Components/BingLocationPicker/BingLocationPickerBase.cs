70c7dd77dc08b252c15201d97f626617b3c17281
MauricePonte <maurice.ponte@student.nhlstudent.com>
2022-08-20 21:20:15 +0200

    Added typescript for displaying Bing map, now the debugging begins :)

---
+++ Client/Components/BingLocationPicker/BingLocationPickerBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace Imkery.Client.Components
+{
+    public class BingLocationPickerBase : ComponentBase
+    {
+        [Inject]
+        private IJSRuntime _JSRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await _JSRuntime.InvokeAsync<Task>("loadMap");
+            await base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
