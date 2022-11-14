9f27e0b6986f4eb470d0314d8de0d3fb990aa63e
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2022-11-14 23:56:02 +0100

    Add Unittesting

---
+++ Imkery.Tests/BootstrapTests.cs
@@ -0,0 +1,30 @@
+﻿using Imkery.API.Client;
+using Imkery.Data.Storage.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imkery.Tests
+{
+    [SetUpFixture]
+    public class BootstrapTests
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+        public BootstrapTests()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ImkeryDbContext>(options =>
+        options.UseInMemoryDatabase("ImkeryTestDb"));
+            services.AddImkeryRepositories();
+            services.AddImkeryAPIClients();
+            services.AddScoped<IImkeryUserProvider, MockUserProvider>();
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+    }
+}
